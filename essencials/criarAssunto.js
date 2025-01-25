c69c9bd2e7277c26df9f7358eecac7725daf50a5
marckosalks <m4rcos.sales@gmail.com>
2025-01-25 02:18:49 -0300

    feat: limitação qauntidade de assuntos

--- essencials/criarAssunto.js
+++ essencials/criarAssunto.js
@@ -1,18 +1,25 @@
-import { inputTitulo, assuntos,  } from "../script.js";
+import { inputTitulo, assuntos, } from "../script.js";
 
-import { criarElemento  } from "../essencials/criarElemento.js";
+import { criarElemento } from "../essencials/criarElemento.js";
 
-
+const LIMITE_ASSUNTOS = 12;
 
 export function criarAssunto() {
-    let titulo = inputTitulo.value.trim();
-    if (titulo) {
-        let assunto = { titulo: titulo, descricao: "" };
-        assuntos.push(assunto);
-        console.log(assuntos);
-        inputTitulo.value = '';
-        criarElemento();
+    if (assuntos.length == LIMITE_ASSUNTOS) {
+        (`Você atingiu o limite máximo de ${LIMITE_ASSUNTOS} tópicos.`);
+        return;
     } else {
-        console.log('O campo de título está vazio.');
+
+        let titulo = inputTitulo.value.trim();
+        if (titulo) {
+            let assunto = { titulo: titulo, descricao: "" };
+            assuntos.push(assunto);
+            console.log(assuntos);
+            inputTitulo.value = '';
+            criarElemento();
+        } else {
+            console.log('O campo de título está vazio.');
+            alert('Algo deu errado.')
+        }
     }
 }
