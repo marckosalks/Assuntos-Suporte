268c73131e10fcb36a9530cb5dfe0366f8b452df
marckosalks <m4rcos.sales@gmail.com>
2025-01-29 00:37:34 -0300

    feat: primeiro modelo darkmode

---
+++ essencials/darkMode.js
@@ -0,0 +1,22 @@
+// Seleciona o botão de alternância
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+
+// Verifica se o usuário já tem uma preferência salva
+if (localStorage.getItem('dark-mode') === 'enabled') {
+  document.body.classList.add('dark-mode');
+} else {
+  darkModeToggle.textContent = 'Modo Escuro';  // Caso não tenha preferência salva
+}
+
+// Adiciona um evento de clique ao botão
+darkModeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+
+  // Atualiza o texto do botão
+  if (document.body.classList.contains('dark-mode')) {
+    localStorage.setItem('dark-mode', 'enabled'); // Salva a preferência
+  } else {
+
+    localStorage.setItem('dark-mode', 'disabled'); // Salva a preferência
+  }
+});
